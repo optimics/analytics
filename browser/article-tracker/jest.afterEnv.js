eb6b90123f58c9fd87b1cd3cf6fd08dc7048db72
Pavel Žák <pavel@zak.global>
2023-07-25 14:12:57 +0200

    test: use page timeout instead of custom

--- browser/article-tracker/jest.afterEnv.js
+++ browser/article-tracker/jest.afterEnv.js
@@ -1,8 +1,8 @@
 import { configureBrowser } from './__jest__/puppeteer'
 import { TextDecoder, TextEncoder } from 'util'
 
-const debug = false
-const graphic = false
+const debug = Boolean(process.env.TEST_DEBUG)
+const graphic = Boolean(process.env.TEST_GRAPHIC)
 const browserRef = configureBrowser({ debug, graphic })
 
 Object.assign(global, { browserRef, TextDecoder, TextEncoder })
