f724d01f2718dc315949ff1856d6c2dbc6373592
Pavel Žák <pavel@zak.global>
2023-08-17 12:15:55 +0200

    feat: deploy ga4 manager to live env

---
+++ tf/run-terraform.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -e
+dir=$1
+
+if [[ "$dir" == "" ]]; then
+  echo "Missing directory path as first argument"
+  exit 4
+fi
+
+if [[ "$TF_VAR_GOOGLE_CREDENTIALS" == "" ]]; then
+  echo "Missing TF_VAR_GOOGLE_CREDENTIALS"
+  exit 2
+fi
+
+if [[ "$TF_VAR_BUCKET_TERRAFORM" == "" ]]; then
+  echo "Missing TF_VAR_BUCKET_TERRAFORM"
+  echo "We use GCP state provider by default, so terraform needs a bucket name"
+  exit 3
+fi
+
+cd $dir
+cred_file="terraform-credentials.json"
+touch $cred_file
+cred_file_path=$(realpath $cred_file)
+export GOOGLE_APPLICATION_CREDENTIALS="$cred_file_path"
+echo -E "$TF_VAR_GOOGLE_CREDENTIALS" >> $cred_file_path
+gcloud auth activate-service-account --key-file=$cred_file_path
+terraform init --backend-config "bucket=$TF_VAR_BUCKET_TERRAFORM"
+terraform apply --input=false --auto-approve
