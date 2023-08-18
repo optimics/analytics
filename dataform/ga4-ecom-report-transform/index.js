9ad59ce33ab689dd4e013f6cd55b380605a215d4
Pavel Žák <pavel@zak.global>
2023-08-18 16:37:20 +0200

    refactor: extract dataform table functions

--- dataform/ga4-ecom-report-transform/index.js
+++ dataform/ga4-ecom-report-transform/index.js
@@ -1,30 +1,9 @@
+const {
+  getNumericDateFormat,
+  getHistoricalTableName,
+} = require('@optimics/dataform-tables')
+
 const helper = require('./eventParams.js')
-
-function formatDate(date) {
-  return String(
-    date.getFullYear() * 1e4 + (date.getMonth() + 1) * 100 + date.getDate(),
-  )
-}
-
-/** Get date from n-days ago and return it in numeric format without time.
- * @example 20230603
- */
-function getNumericDateFormat(daysAgo) {
-  const date = new Date()
-  date.setDate(date.getDate() - daysAgo)
-  return formatDate(date)
-}
-
-/** Construct GA4 table name based on the date
- * @param {number} daysAgo
- * @returns string
- */
-function getHistoricalTableName(daysAgo) {
-  const prefix_universal = 'events_'
-  const prefix_intraday = daysAgo === 1 ? 'intraday_' : ''
-  const formattedDate = getNumericDateFormat(daysAgo)
-  return `${prefix_universal}${prefix_intraday}${formattedDate}`
-}
 
 /** Return intraday table specific for utm source customers as a string
  * @param config.ga4Dataset The Fully Qualified dataset name prefixed with GCP project name
--- dataform/utm-source-filter/index.js
+++ dataform/utm-source-filter/index.js
@@ -1,28 +1,4 @@
-function formatDate(date) {
-  return String(
-    date.getFullYear() * 1e4 + (date.getMonth() + 1) * 100 + date.getDate(),
-  )
-}
-
-/** Get date from n-days ago and return it in numeric format without time.
- * @example 20230603
- */
-function getNumericDateFormat(daysAgo) {
-  const date = new Date()
-  date.setDate(date.getDate() - daysAgo)
-  return formatDate(date)
-}
-
-/** Construct GA4 table name based on the date
- * @param {number} daysAgo
- * @returns string
- */
-function getHistoricalTableName(daysAgo) {
-  const prefix_universal = 'events_'
-  const prefix_intraday = daysAgo === 1 ? 'intraday_' : ''
-  const formattedDate = getNumericDateFormat(daysAgo)
-  return `${prefix_universal}${prefix_intraday}${formattedDate}`
-}
+const { getHistoricalTableName } = require('@optimics/dataform-tables')
 
 /** Return intraday table specific for utm source customers as a string
  * @param config.ga4Dataset The Fully Qualified dataset name prefixed with GCP project name
