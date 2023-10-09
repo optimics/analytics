19e07865bd520409b66b255eedd2cefa1763f5b4
Pavel Žák <pavel@zak.global>
2023-10-09 03:57:17 +0200

    chore: use terraform deploy workspaces

--- tf/run-terraform.sh
+++ tf/run-terraform.sh
@@ -2,6 +2,7 @@
 
 set -e
 dir=$1
+workspace=$2
 
 if [[ "$dir" == "" ]]; then
   echo "Missing directory path as first argument"
@@ -27,4 +28,14 @@ export GOOGLE_APPLICATION_CREDENTIALS="$cred_file_path"
 echo -E "$TF_VAR_GOOGLE_CREDENTIALS" >> $cred_file_path
 gcloud auth activate-service-account --key-file=$cred_file_path
 terraform init --backend-config "bucket=$TF_VAR_BUCKET_TERRAFORM"
+
+if [[ "$workspace" != "" ]]; then
+  set +e
+  terraform workspace select $workspace
+  if [[ "$?" != "0" ]]; then
+    terraform workspace new $workspace
+  fi
+  set -e
+fi
+
 terraform apply --input=false --auto-approve
