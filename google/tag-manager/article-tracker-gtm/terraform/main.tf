53650eba81a1aebdc4623d53e8fc55461b5dba42
Pavel Žák <pavel@zak.global>
2023-10-09 03:57:17 +0200

    chore: use terraform deploy workspaces

--- google/tag-manager/article-tracker-gtm/terraform/main.tf
+++ google/tag-manager/article-tracker-gtm/terraform/main.tf
@@ -4,15 +4,6 @@ module "npm" {
   source = "github.com/optimics/forge/packages/repo/terraform/npm"
 }
 
-module "bucket_scripts" {
-  common   = local.common
-  location = local.common.region
-  name     = local.common.bucket_scripts
-  origins  = ["*"]
-  public   = true
-  source   = "../../../../tf/bucket"
-}
-
 module "scripts_upload" {
   source      = "../../../../tf/bucket_dir"
   bucket_name = local.common.bucket_scripts
